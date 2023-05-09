d60beaa51f9fc0321924e27d84566bffa363e371
Micah Galizia <micahgalizia@gmail.com>
2023-05-08 21:15:04 -0400

    0.0.13 (#37)
    * try to fix combined api url
    * closes #36

--- src/middleware/EnvironmentMiddleware.ts
+++ src/middleware/EnvironmentMiddleware.ts
@@ -2,16 +2,26 @@ import { Middleware } from 'redux';
 import axios from 'axios';
 
 export const EnvironmentMiddleware: Middleware = storeAPI => next => action => {
-    if (action.type === 'environment/config') {
-        console.log(`Requesting config`);
-        axios.get('/env.json').then(data => {
-            action.payload = data;
-            return next(action);
-        }).catch(reason => {
-            // TODO trigger an error
-            console.error(`FAILED TO FETCH ${JSON.stringify(reason)}`)
-        });
-    } else {
-        return next(action);
-    }
+  if (action.type === 'environment/config') {
+    console.log(`Requesting config`);
+    axios.get('/env.json').then(data => {
+      action.payload = data;
+
+      // small hack here for when we're running in combined docker.
+      // saas images will have non localhost values returned by through a
+      // k8s config map. Otherwise, if we're running on some non-localhost
+      // value with our API_URL configured to localhost, just use combined
+      // protocol/host/port as the base (for the combined docker image)
+      const inferredUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+      if (action.payload.data.API_URL === "http://localhost:3000" && window.location.hostname !== 'localhost') {
+        action.payload.data.API_URL = inferredUrl;
+      }
+      return next(action);
+    }).catch(reason => {
+      // TODO trigger an error
+      console.error(`FAILED TO FETCH ${JSON.stringify(reason)}`)
+    });
+  } else {
+    return next(action);
+  }
 }
