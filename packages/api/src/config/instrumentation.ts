1e212a206e26d4d4ceb3ac45bfccabcdf6b685f4
Micah Galizia <micahgalizia@gmail.com>
2024-04-23 22:10:30 -0400

    feat: add counter for mongoose connection

--- packages/api/src/config/instrumentation.ts
+++ packages/api/src/config/instrumentation.ts
@@ -1,16 +1,17 @@
 /*instrumentation.ts*/
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from "@opentelemetry/sdk-metrics";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 
+// opentelemetry-collector-service:4318
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  // traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    // exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPMetricExporter(),
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
