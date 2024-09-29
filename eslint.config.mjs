827a885670adea62fc7760fe72f700fcbda6b6a6
Micah Galizia <micahgalizia@gmail.com>
2024-09-29 09:23:06 -0400

    feat: add react but it doesn't seem to do anything...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -18,16 +20,25 @@ export default tseslint.config(
     },
   },
   {
-    files: [
-      // "packages/mui/src/index.tsx",
-      "packages/mui/**/*.tsx",
-      "packages/mui/**/*.ts"
-    ],
+    files: ["packages/mui/**/*.tsx", "packages/mui/**/*.ts"],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    plugins: {
+      react,
+    },
     languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
       parserOptions: {
-        ecmaFeatures: { modules: true },
         ecmaVersion: 'latest',
         project: './packages/mui/tsconfig.json',
+        ecmaFeatures: {
+          modules: true,
+          jsx: true,
+        },
+        globals: {
+          ...globals.serviceworker,
+          ...globals.browser,
+        }
       },
     }
   }
