e910029e60e18604a1de1d77d514eb331a35ac30
Micah Galizia <micahgalizia@gmail.com>
2024-10-27 11:03:42 -0400

    feat: round asset edges in AssetPanel

--- packages/mui/src/components/AssetPanelComponent/AssetPanelComponent.tsx
+++ packages/mui/src/components/AssetPanelComponent/AssetPanelComponent.tsx
@@ -1,7 +1,8 @@
 import { Asset } from "@micahg/tbltp-common";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField, Tooltip } from "@mui/material";
 import { useSelector } from "react-redux";
 import { AppReducerState } from "../../reducers/AppReducer";
+import styles from "./AssetPanelComponent.module.css";
 
 interface AssetPanelComponentProps {
   key: number;
@@ -29,19 +30,42 @@ const AssetPanelComponent = ({
         justifyContent: "center",
         boxShadow: 4,
         borderRadius: 2,
-        padding: 1,
         minHeight: "25vh",
         width: "25vw",
       }}
     >
-      <img src={imgUrl} alt={asset.name} />
+      <img src={imgUrl} alt={asset.name} className={styles.asset} />
       {!readonly && (
-        <TextField
-          id="name"
-          label="Name"
-          variant="standard"
-          defaultValue={asset.name}
-        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: "1em",
+            padding: 1,
+          }}
+        >
+          <TextField
+            id="name"
+            label="Name"
+            variant="standard"
+            defaultValue={asset.name}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "right",
+              gap: "1em",
+            }}
+          >
+            <Tooltip title="Save your changes to the asset">
+              <span>
+                <Button variant="text">Save</Button>
+              </span>
+            </Tooltip>
+          </Box>
+        </Box>
       )}
     </Box>
   );
