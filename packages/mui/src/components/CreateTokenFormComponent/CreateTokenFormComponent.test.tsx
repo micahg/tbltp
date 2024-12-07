56dfdb38a058b03b5c93f8f3aeca01847a21ce5a
Micah Galizia <micahgalizia@gmail.com>
2024-12-07 08:48:15 -0500

    fix: test lint

--- packages/mui/src/components/CreateTokenFormComponent/CreateTokenFormComponent.test.tsx
+++ packages/mui/src/components/CreateTokenFormComponent/CreateTokenFormComponent.test.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import CreateTokenFormComponent from './CreateTokenFormComponent';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import CreateTokenFormComponent from "./CreateTokenFormComponent";
 
-describe('<CreateTokenFormComponent />', () => {
-  test('it should mount', () => {
+describe("<CreateTokenFormComponent />", () => {
+  test("it should mount", () => {
     render(<CreateTokenFormComponent />);
 
-    const createTokenFormComponent = screen.getByTestId('CreateTokenFormComponent');
+    const createTokenFormComponent = screen.getByTestId(
+      "CreateTokenFormComponent",
+    );
 
     expect(createTokenFormComponent).toBeInTheDocument();
   });
