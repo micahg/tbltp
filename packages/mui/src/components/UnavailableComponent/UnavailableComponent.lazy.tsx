0ecab98d3120d86bbbb006ad384afa1d9e093f2a
Micah Galizia <micahgalizia@gmail.com>
2024-12-27 19:43:21 -0500

    fix: correct lint

--- packages/mui/src/components/UnavailableComponent/UnavailableComponent.lazy.tsx
+++ packages/mui/src/components/UnavailableComponent/UnavailableComponent.lazy.tsx
@@ -1,8 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react";
 
-const LazyUnavailableComponent = lazy(() => import('./UnavailableComponent'));
+const LazyUnavailableComponent = lazy(() => import("./UnavailableComponent"));
 
-const UnavailableComponent = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
+const UnavailableComponent = (
+  props: JSX.IntrinsicAttributes & { children?: React.ReactNode },
+) => (
   <Suspense fallback={null}>
     <LazyUnavailableComponent {...props} />
   </Suspense>
