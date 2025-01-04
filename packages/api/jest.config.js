884f32a4a45d4e13d3e3ab9af029b67d1ecf029e
Micah Galizia <micahgalizia@gmail.com>
2025-01-04 09:53:17 -0500

    feat: update to mongo 8.0.4 (#352)
    * feat: update to mongo 8.0.4

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -1,10 +1,10 @@
-process.env.MONGOMS_VERSION="7.0.0";
-process.env.MONGOMS_DOWNLOAD_URL="https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-7.0.0.tgz";
+process.env.MONGOMS_VERSION = "8.0.4";
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   transform: {
-  '^.+\\.(ts|tsx)?$': 'ts-jest',
-  "^.+\\.(js|jsx)$": "babel-jest",
-}};
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+};
