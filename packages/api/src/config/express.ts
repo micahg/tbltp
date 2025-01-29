e2cbf4d3152ee5bd598488b887ffd44e47bd73d8
Micah Galizia <micahgalizia@gmail.com>
2025-01-28 22:00:17 -0500

    feat: set visibility on instance token creation

--- packages/api/src/config/express.ts
+++ packages/api/src/config/express.ts
@@ -113,6 +113,7 @@ function schemaErrorCheck(
 ) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    log.error("Schema validation failed", { errors: errors.array() });
     return res.sendStatus(400);
   }
   next();
--- packages/common/src/token.ts
+++ packages/common/src/token.ts
@@ -9,7 +9,6 @@ export const MIN_HP = 1;
 export interface Token {
   _id?: string;
   name: string;
-  visible: boolean;
   asset?: string;
   hitPoints?: number;
 }
