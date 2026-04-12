b109ce521bc41d5d3f9050a72c89c948ce896e2b
Micah Galizia <micahgalizia@gmail.com>
2026-04-11 20:32:23 -0400

    chore: cleanup

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -3,7 +3,6 @@ process.env.MONGOMS_VERSION = "8.0.4";
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  globalSetup: "<rootDir>/test/globalSetup.js",
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
