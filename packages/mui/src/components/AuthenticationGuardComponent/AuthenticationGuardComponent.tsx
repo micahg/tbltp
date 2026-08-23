53457f8cf81ed46f6bef08a7c73298894d76aef1
Micah Galizia <micahgalizia@gmail.com>
2026-08-23 09:10:13 -0400

    fix: tighten up auth guard

--- packages/mui/src/components/AuthenticationGuardComponent/AuthenticationGuardComponent.tsx
+++ packages/mui/src/components/AuthenticationGuardComponent/AuthenticationGuardComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {
   clearAccessTokenGetter,
@@ -25,6 +25,9 @@ const AuthenticationGuardComponent = ({
   // we'd need to skip this query until we have the environmentConfig
   const { data: noAuthConfig } = useGetNoAuthConfigQuery();
 
+  // keep a ref to avoid double-authenticating
+  const isRedirectingRef = useRef(false);
+
   useEffect(() => {
     let getter: (() => Promise<string | null>) | null;
     if (noAuthConfig?.noauth === true) {
@@ -38,12 +41,28 @@ const AuthenticationGuardComponent = ({
     return () => clearAccessTokenGetter(getter);
   }, [getAccessTokenSilently, isAuthenticated, noAuthConfig]);
 
+  useEffect(() => {
+    if (
+      noAuthConfig?.noauth === true ||
+      isAuthenticated ||
+      isRedirectingRef.current
+    ) {
+      return;
+    }
+    isRedirectingRef.current = true;
+    loginWithRedirect();
+  }, [isAuthenticated, loginWithRedirect, noAuthConfig?.noauth]);
+
+  // if configured to operate without auth, just render the children
+  if (noAuthConfig?.noauth === true) {
+    return <>{children}</>;
+  }
+
   if (isLoading) {
     return <AuthLoadingComponent />;
   }
 
   if (!isAuthenticated) {
-    loginWithRedirect();
     return <AuthLoadingComponent />;
   }
 
